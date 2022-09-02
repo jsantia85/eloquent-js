1d1899e40347b6dc5bc388ef57e104dd77812d33
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-02 10:33:49 -0400

    solve problem 3 with extra feature

--- Chapter-2/app.js
+++ Chapter-2/app.js
@@ -27,9 +27,40 @@
 
 // solution in book
 
-for (let n = 1; n <= 100; n++) {
-  let output = "";
-  if (n % 3 == 0) output += "Fizz";
-  if (n % 5 == 0) output += "Buzz";
-  console.log(output || n);
+// for (let n = 1; n <= 100; n++) {
+//   let output = "";
+//   if (n % 3 == 0) output += "Fizz";
+//   if (n % 5 == 0) output += "Buzz";
+//   console.log(output || n);
+// }
+
+// Exercise 3: Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+
+// Passing this string to console.log should show something like this:
+// # # # #
+// # # # # 
+// # # # #
+// # # # # 
+// # # # #
+// # # # # 
+// # # # #
+// # # # #
+// When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+
+function chessboard(size){
+
+  let board = "";
+
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      if ((x + y) % 2 == 0) {
+        board += " ";
+      } else {
+        board += "#";
+      }
+    }
+    board += "\n";
+  }
+  return board
 }
+console.log(chessboard(10));
