22fb9808ae35d4b5be9f9c0abd047180301683f7
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-02 10:21:14 -0400

    solve exercise 2

--- Chapter-2/app.js
+++ Chapter-2/app.js
@@ -15,3 +15,12 @@
 
 // When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 
+for (let i = 1; i < 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz', i)
+    } else if (i % 3 === 0) {
+      console.log('Fizz', i)
+    } else if (i % 5 === 0) {
+      console.log('Buzz', i)
+    }
+}
